12297c200c8e6f0021dd7f74de6c6636c1fa26c1
Oussoubi <90727185+Oussoubi@users.noreply.github.com>
2022-04-05 12:53:11 +0100

    Update MainWindow.h

--- myzork/MainWindow.h
+++ myzork/MainWindow.h
@@ -2,36 +2,28 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QListWidget>
-#include "ui_MainWindow.h"
-#include "ZorkUL.h"
 
-namespace Ui {
-    class MainWindow;
-}
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow{
+class MainWindow : public QMainWindow
+{
     Q_OBJECT
+    friend class Character;
     friend class Room;
+    friend class ZorkUL;
 
 public:
-    explicit MainWindow(QWidget *parent = nullptr);
+    MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-
-private:
-    Ui::MainWindow *ui;
-    ZorkUL *zork = new ZorkUL();
-    void goRoom(string direction);
 
 private slots:
     void on_pushButton_clicked();
+
     void on_pushButton_2_clicked();
-    void on_pushButton_3_clicked();
-    void on_pushButton_4_clicked();
-    void on_pushButton_5_clicked();
-    void on_pushButton_6_clicked();
 
+private:
+    Ui::MainWindow *ui;
 };
-
-
 #endif // MAINWINDOW_H
